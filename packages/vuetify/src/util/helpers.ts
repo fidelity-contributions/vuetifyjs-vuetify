89e0ebb68de7f9ae7ec74b1b0cdf7071cd1dad9b
jcjp <tchamver@hotmail.com>
2025-03-29 20:59:28 +0800

    chore(VFileUpload): resolve lint errors

--- packages/vuetify/src/util/helpers.ts
+++ packages/vuetify/src/util/helpers.ts
@@ -756,6 +756,5 @@ export function isPrimitive (value: unknown): value is Primitive {
 
 export function filterFilesByAcceptType (acceptType: string, files: null | FileList): File[] {
   if (!acceptType || !files) return []
-  return Array.from(files).filter((file) => file.type === acceptType)
+  return Array.from(files).filter(file => file.type === acceptType)
 }
-
